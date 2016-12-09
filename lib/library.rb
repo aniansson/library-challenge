dbf5ffd10a717225451b7ff84e6187686da1d6f3
blondabambi <ebba@aniansson.se>
2016-12-09 21:52:58 +0100

    change return date

--- lib/library.rb
+++ lib/library.rb
@@ -1,8 +1,9 @@
 class Library
   attr_accessor :collection
 
-  def initialize
-    @collection = YAML.load_file('./lib/data.yml')
+  def initialize(file_name = './lib/data.yml')
+    @file_name = file_name
+    @collection = YAML.load_file(file_name)
   end
 
   def search_by_title(book_title)
@@ -19,5 +20,15 @@ class Library
     results = []
     available.each { |book|                       results.push(book[:item][:title]) }
     return results
+  end
+
+  def rent_a_book(book)
+    collection.each do |search_book|
+      if search_book == book
+        search_book[:available] = false
+        search_book[:return_date] = Date.today.next_month(1).strftime('%d/%m')
+      end
+    end
+    File.open(@file_name, 'w') { |f| f.write collection.to_yaml }
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -8,42 +8,51 @@
 require './lib/library.rb'
 
 describe Library do
+  let(:fixture_file) { './spec/fixtures/data.yml' }
+  before(:each) do
+    collection = YAML.load_file('./lib/data.yml')
+    File.open(fixture_file, 'w') { |f| f.write collection.to_yaml }
+  end
+
   it 'should have a collection of books' do
     collection = YAML.load_file('./lib/data.yml')
     expect(subject.collection).to eq collection
   end
 
-  it 'should allow people to search for books by title' do
-    book_title = 'Pippi Långstrump'
-    expected_book = {
-      item: {
-        title: 'Pippi Långstrump',
-        author: 'Astrid Lindgren'
-      },
-      available: true,
-      return_date: nil
-    }
+  context '#search_by_title' do
+    it 'should allow to search a book by its title' do
+      book_title = 'Pippi Långstrump'
+      expected_book = {
+        item: {
+          title: 'Pippi Långstrump',
+          author: 'Astrid Lindgren'
+        },
+        available: true,
+        return_date: nil
+      }
 
-    actual_book = subject.search_by_title(book_title)
+      actual_book = subject.search_by_title(book_title)
 
-    expect(actual_book).to eq expected_book
+      expect(actual_book).to eq expected_book
+    end
+
+    it 'should work with a different title' do
+      book_title = 'Alfons och soldatpappan'
+      expected_book = {
+        item: {
+          title: 'Alfons och soldatpappan',
+          author: 'Gunilla Bergström'
+        },
+        available: true,
+        return_date: nil
+      }
+
+      actual_book = subject.search_by_title(book_title)
+
+      expect(actual_book).to eq expected_book
+    end
   end
 
-  it 'should allow people to search for books by title with a different title' do
-    book_title = 'Alfons och soldatpappan'
-    expected_book = {
-      item: {
-        title: 'Alfons och soldatpappan',
-        author: 'Gunilla Bergström'
-      },
-      available: true,
-      return_date: nil
-    }
-
-    actual_book = subject.search_by_title(book_title)
-
-    expect(actual_book).to eq expected_book
-  end
 
   it 'should allow people to search for books by author' do
     book_author = 'Astrid Lindgren'
@@ -72,6 +81,37 @@ describe Library do
 
   end
 
-#   it 'should allow people to checkout books'
-#   it 'ensures that the return date is correct when a person borrows a book'
+  it 'should allow people to checkout books' do
+
+    library = Library.new(fixture_file)
+    book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+    library.rent_a_book(book)
+    found_book = library.search_by_title(book[:item][:title])
+
+    expect(found_book[:available]).to eq false
+  end
+
+  it 'ensures that the return date is correct when a person borrows a book' do
+    expected_return_date = Date.today.next_month(1).strftime('%d/%m')
+    library = Library.new(fixture_file)
+    book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+    library.rent_a_book(book)
+    found_book = library.search_by_title(book[:item][:title])
+
+    expect(found_book[:return_date]).to eq expected_return_date
+  end
 end
