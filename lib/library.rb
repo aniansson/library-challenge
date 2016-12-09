c0d23e53214bad6006c3760ca0c93a5374983538
blondabambi <ebba@aniansson.se>
2016-12-09 12:38:11 +0100

    our library can search books by title and author

--- lib/library.rb
+++ lib/library.rb
@@ -4,4 +4,12 @@ class Library
   def initialize
     @collection = YAML.load_file('./lib/data.yml')
   end
+
+  def search_by_title(book_title)
+    collection.detect { |book| book[:item][:title].include? book_title }
+  end
+
+  def search_by_author(book_author)
+    collection.detect { |book| book[:item][:author].include? book_author }
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -13,8 +13,56 @@ describe Library do
     expect(subject.collection).to eq collection
   end
 
-#   it 'should allow people to search for books by title'
-#   it 'should allow people to search for books by author'
+  it 'should allow people to search for books by title' do
+    library = Library.new
+    book_title = 'Pippi Långstrump'
+    expected_book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_title(book_title)
+
+    expect(actual_book).to eq expected_book
+  end
+
+  it 'should allow people to search for books by title with a different title' do
+    library = Library.new
+    book_title = 'Alfons och soldatpappan'
+    expected_book = {
+      item: {
+        title: 'Alfons och soldatpappan',
+        author: 'Gunilla Bergström'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_title(book_title)
+
+    expect(actual_book).to eq expected_book
+  end
+
+  it 'should allow people to search for books by author' do
+    library = Library.new
+    book_author = 'Astrid Lindgren'
+    expected_book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_author(book_author)
+
+    expect(actual_book).to eq expected_book
+  end
 #   it 'should allow people to search for all available books'
 #   it 'should allow people to checkout books'
 #   it 'ensures that the return date is correct when a person borrows a book'
